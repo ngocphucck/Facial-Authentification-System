21729fe07446d1dfd105822ceaa14e1b1a119744
manhph2211 <maxph22112000@gmail.com>
2022-09-17 11:20:04 +0700

    ..

--- deployment/app.py
+++ deployment/app.py
@@ -4,6 +4,7 @@ from PIL import Image, ImageEnhance
 import numpy as np
 import os
 import time
+import json
 
 
 @st.cache
@@ -19,23 +20,6 @@ def detect_faces(our_image):
     # Detect faces
     faces = None
     return img, faces
-
-
-def detect_eyes(our_image):
-    new_img = np.array(our_image.convert('RGB'))
-    img = cv2.cvtColor(new_img, 1)
-    gray = cv2.cvtColor(new_img, cv2.COLOR_BGR2GRAY)
-    eyes = None
-    return img, eyes
-
-
-def detect_smiles(our_image):
-    new_img = np.array(our_image.convert('RGB'))
-    img = cv2.cvtColor(new_img, 1)
-    gray = cv2.cvtColor(new_img, cv2.COLOR_BGR2GRAY)
-    # Detect Smiles
-    smiles = None
-    return img
 
 
 def cartonize_image(our_image):
@@ -60,6 +44,12 @@ def cannize_image(our_image):
     img = cv2.GaussianBlur(img, (11, 11), 0)
     canny = cv2.Canny(img, 100, 150)
     return canny
+
+
+def verify():
+    '''dump example
+    '''
+    return json.load(open("deployment/assets/target_imgs/20180134/20180134.json",'r'))
 
 
 def main():
@@ -101,88 +91,58 @@ def main():
             new_upload_path = os.path.join(user_folder, time.strftime("%Y%m%d%H%M%S.jpg"))
             cv2.imwrite(new_upload_path, cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR))
 
-
+            user_info_path = os.path.join(user_folder, f"{user_code}.json")
+            user_info = {
+                "name": user_name,
+                "user_code": user_code,
+                "user_dob": user_dob,
+                "img_num": len([name for name in os.listdir(f'deployment/assets/target_imgs/{user_code}') if os.path.isfile(name) and name.endswith("jpg")])
+            }
+            with open(user_info_path,'w') as f:
+                json.dump(user_info, f, indent=2)
 
     if choice == 'Verify':
         st.subheader("Face Verification")
-
-        image_file = st.file_uploader(
-            "Upload Image", type=['jpg', 'png', 'jpeg'])
-
-        if image_file is not None:
-            our_image = Image.open(image_file)
-            st.text("Original Image")
-            # st.write(type(our_image))
-            st.image(our_image)
+        image = st.camera_input("Take a picture")
+        if image is not None:
+            image = Image.open(image)
 
             enhance_type = st.sidebar.radio(
                 "Enhance Type", ["Original", "Gray-Scale", "Contrast", "Brightness", "Blurring"])
+                
             if enhance_type == 'Gray-Scale':
-                new_img = np.array(our_image.convert('RGB'))
+                new_img = np.array(image.convert('RGB'))
                 img = cv2.cvtColor(new_img, 1)
-                gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-                # st.write(new_img)
-                st.image(gray)
+                result = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+                st.image(result)
+
             elif enhance_type == 'Contrast':
                 c_rate = st.sidebar.slider("Contrast", 0.5, 3.5)
-                enhancer = ImageEnhance.Contrast(our_image)
-                img_output = enhancer.enhance(c_rate)
-                st.image(img_output)
+                enhancer = ImageEnhance.Contrast(image)
+                result = enhancer.enhance(c_rate)
+                st.image(result)
 
             elif enhance_type == 'Brightness':
                 c_rate = st.sidebar.slider("Brightness", 0.5, 3.5)
-                enhancer = ImageEnhance.Brightness(our_image)
-                img_output = enhancer.enhance(c_rate)
-                st.image(img_output)
+                enhancer = ImageEnhance.Brightness(image)
+                result = enhancer.enhance(c_rate)
+                st.image(result)
 
             elif enhance_type == 'Blurring':
-                new_img = np.array(our_image.convert('RGB'))
+                new_img = np.array(image.convert('RGB'))
                 blur_rate = st.sidebar.slider("Blurring", 0.5, 3.5)
                 img = cv2.cvtColor(new_img, 1)
-                blur_img = cv2.GaussianBlur(img, (11, 11), blur_rate)
-                st.image(blur_img)
-            elif enhance_type == 'Original':
+                result = cv2.GaussianBlur(img, (11, 11), blur_rate)
+                st.image(result)
 
-                st.image(our_image, width=300)
             else:
-                st.image(our_image, width=300)
+                # st.image(image, width=300)
+                result = image
 
-        # Face Detection
-        task = ["Faces", "Smiles",
-                "Eyes", "Cannize", "Cartonize"]
-        feature_choice = st.sidebar.selectbox("Find Features", task)
         if st.button("Process"):
-
-            if feature_choice == 'Faces':
-                result_img, result_faces = detect_faces(our_image)
-                st.image(result_img)
-
-                st.success("Found {} faces".format(len(result_faces)))
-            elif feature_choice == 'Smiles':
-                result_img = detect_smiles(our_image)
-                st.image(result_img)
-
-            elif feature_choice == 'Eyes':
-                result_img, result_eyes = detect_eyes(our_image)
-                st.success("Found {} Eyes".format(len(result_eyes)))
-                st.image(result_img)
-
-            elif feature_choice == 'Cartonize':
-                result_img = cartonize_image(our_image)
-                st.image(result_img)
-
-            elif feature_choice == 'Cannize':
-                result_canny = cannize_image(our_image)
-                st.image(result_canny)
-
-            elif feature_choice == 'Eyes and Faces':
-                result_img, result_faces = detect_faces(our_image)
-                st.image(result_img)
-                result_img = detect_eyes(our_image)
-                st.image(result_img)
-                st.success("Found {} faces and {} eyes".format(
-                    len(result_faces), len(result_eyes)))
-
+            with st.spinner(text="This may take a moment..."):
+                data = verify()
+                st.write(data)
 
 
 if __name__ == '__main__':
